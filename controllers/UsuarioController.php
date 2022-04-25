3a4f90208c9d732465b0e3f73293c7848605038b
rbocache <raulbocache@gmail.com>
2022-04-25 18:31:44 +0200

    Añadir métodos registro() y save() en la clase usuarioController

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -5,4 +5,12 @@ class usuarioController {
     public function index(){
         echo "Controlador de los usuarios";
     }
+
+    public function registro() {
+        require_once 'views/usuario/registro.php';
+    }
+
+    public function save() {
+
+    }
 }
