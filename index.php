294dc01ea96a9e99169a9b1499549d2e37f86256
rbocache <raulbocache@gmail.com>
2022-05-04 19:17:53 +0200

    Añado control de url erronea y carga de controlador y acción por defecto

--- index.php
+++ index.php
@@ -9,9 +9,10 @@ function show_error() {
     $error->index();
 }
 
-if(isset($_GET['controller'])) {
-    
+if (isset($_GET['controller'])) {    
     $nombre_controlador = $_GET['controller'].'Controller';
+} elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+    $nombre_controlador = controller_default;
 } else {
     show_error();
     exit();
@@ -20,9 +21,12 @@ if(isset($_GET['controller'])) {
 if(class_exists($nombre_controlador)) {
     $controlador = new $nombre_controlador();
 
-    if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
+    if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
         $action = $_GET['action'];
         $controlador->$action();
+    } elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+        $action_default = action_default;
+        $controlador->action_default();
     } else {
         show_error();
     }
